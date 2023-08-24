b26b22fffc39b85017754a6b8f5466491d5002b5
EGY-Temble <fyotol@gmail.com>
2023-08-24 04:22:50 +0300

    Add a New Page Has Name: items

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <div className="text-center text-4xl text-zinc-500 w-full mt-10">
+        <p>Nothing Here</p>
+      </div>
+    </>
+  );
+}
