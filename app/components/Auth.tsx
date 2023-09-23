501654f284bf7635b15d5f65f3e051721cf4dbe4
EGY-Temble <fyotol@gmail.com>
2023-09-23 09:13:41 +0300

    Edit With Auth Component

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -1,41 +1,64 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { setLoading } from "../redux/reducers/userSlice";
 
 export default function Auth({ children, type }: any) {
-  const Store = useSelector((state: any) => state.user);
-  const dispatch = useDispatch();
+  const [Load, SetLoad] = useState<boolean>();
   const router: any = useRouter();
+  const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(setLoading(Load));
     const user_id: any = localStorage.getItem("u_id");
-
     switch (type) {
       case "login":
-        if (user_id?.length) return router?.push("/page/sales");
-        else if (!user_id?.length) return;
-        else return router.push("/");
+        if (user_id?.length) {
+          SetLoad(true);
+          return router?.push("/page/sales");
+        } else if (!user_id?.length) {
+          SetLoad(false);
+          return;
+        } else {
+          SetLoad(false);
+          return router.push("/");
+        }
         break;
       case "user":
-        if (user_id?.length) return;
-        else if (!user_id?.length) return router.push("/page/account/login");
-        else return router.push("/");
+        if (user_id?.length) {
+          SetLoad(false);
+          return;
+        } else if (!user_id?.length) {
+          SetLoad(true);
+          return router.push("/page/account/login");
+        } else {
+          SetLoad(false);
+          return router.push("/");
+        }
         break;
       case "admin":
-        if (user_id == "12123") return;
-        else if (user_id != "12123" || !user_id?.length) {
-          if (user_id?.length) return router.push("/page/sales");
-          else return router.push("/page/account/login");
-        } else if (user_id?.length) return router.push("/page/sales");
-        else return router.push("/");
+        if (user_id == "12123") {
+          SetLoad(false);
+          return;
+        } else if (user_id != "12123" || !user_id?.length) {
+          if (user_id?.length) {
+            SetLoad(true);
+            return router.push("/page/sales");
+          } else {
+            SetLoad(true);
+            return router.push("/page/account/login");
+          }
+        } else if (user_id?.length) {
+          SetLoad(true);
+          return router.push("/page/sales");
+        } else {
+          SetLoad(false);
+          return router.push("/");
+        }
         break;
     }
-  }, [router, type]);
+  }, [Load, dispatch, router, type]);
 
-  return (
-    <>
-      <Loading>{children}</Loading>
-    </>
-  );
+  return <Loading>{Load ? "" : children}</Loading>;
 }
